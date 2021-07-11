1d2d2adda3eed3b2b3c1b1e89ae1a8d7940757d7
Marlon Muñoz <marlonjavi@gmail.com>
2021-07-11 12:04:26 -0500

    template html is created and a HTTP server is implemented too

--- main/main.go
+++ main/main.go
@@ -1,11 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("template/index.html"))
+}
+
+func a(res http.ResponseWriter, req *http.Request) {
+	err := tpl.Execute(res, nil)
+	if err != nil {
+		log.Fatalln("template didn't execute: ", err)
+	}
+
+}
 
 func main() {
 
-	greetings := "Test GitHub"
-
-	fmt.Println(greetings)
+	http.Handle("/resources/", http.StripPrefix("/resources", http.FileServer(http.Dir("public"))))
+	http.Handle("/tmp/", http.StripPrefix("/tmp", http.FileServer(http.Dir("template"))))
+	http.Handle("/", http.HandlerFunc(a))
+	http.ListenAndServe("Localhost:8080", nil)
 
 }
